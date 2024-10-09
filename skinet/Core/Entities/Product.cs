8ef00b60973a9afaafc1bc816d0588d2baf9f9bb
Laura <poleuca.laura@gmail.com>
2024-10-09 12:26:32 +0300

    skinet init

---
+++ skinet/Core/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Product: BaseEntity
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public decimal Price { get; set; }
+        public required string PictureUrl { get; set; }
+        public required string Type { get; set;}
+        public required string Brand{ get; set; }
+        public int QuantityInStock {  get; set; }
+    }
+}
---
+++ skinet/Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
