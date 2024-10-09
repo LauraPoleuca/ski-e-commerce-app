0b83f0eb0352dadbd3718ae7abbe1daf646a6584
Laura <poleuca.laura@gmail.com>
2024-10-09 16:47:31 +0300

    add docker container for ms sql, add migrations, fix ef design dependency

--- skinet/Infrastructure/Data/StoreContext.cs
+++ skinet/Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities;
+using Infrastructure.Config;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,12 @@ namespace Infrastructure.Data
     public class StoreContext(DbContextOptions options) : DbContext(options)
     {
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+        }
     }
 }
